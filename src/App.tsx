40fd8bd2603868b04cd3db62fb5997f016a341e7
Peter Khalil <peter.khalil.fahmy@gmail.com>
2025-07-19 23:19:04 +0300

    🎉 Setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@components/ui/button';
+const App = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-background text-foreground">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
