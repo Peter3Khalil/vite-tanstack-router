eadf3739a7c6d6b3c96c17510761ab4173ec1e0c
Peter Khalil <peter.khalil.fahmy@gmail.com>
2025-07-21 21:59:11 +0300

    🐛 Disable preload in language switcher component

--- src/components/shared/language-switcher.tsx
+++ src/components/shared/language-switcher.tsx
@@ -3,7 +3,10 @@ import { Link, usePathname } from '@/i18n/routing';
 import { useLocation } from '@tanstack/react-router';
 import i18next from 'i18next';
 import { type FC } from 'react';
-type Props = Omit<React.ComponentProps<typeof Link>, 'href' | 'locale'>;
+type Props = Omit<
+  React.ComponentProps<typeof Link>,
+  'href' | 'locale' | 'preload'
+>;
 const LanguageSwitcher: FC<Props> = ({ className, children, ...props }) => {
   const locale = i18next.language;
   const pathname = usePathname();
@@ -12,6 +15,7 @@ const LanguageSwitcher: FC<Props> = ({ className, children, ...props }) => {
     <Link
       to={`${pathname}${searchStr}` as never}
       locale={locale === 'en' ? 'ar' : 'en'}
+      preload={false}
       className={className}
       {...props}
     >
