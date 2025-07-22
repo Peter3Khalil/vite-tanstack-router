fbf24b8847044b63cd4b4812af437caa3fbb01e9
Peter Khalil <peter.khalil.fahmy@gmail.com>
2025-07-22 14:25:22 +0300

    ♻️ Update type-check script to use TypeScript project references

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
+  '*': () => 'tsc -b --noEmit',
   '*.{js,ts,jsx,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{ts,tsx}': ['tsc --noEmit'],
   '*.{json,md,css,scss}': ['prettier --write'],
 };
